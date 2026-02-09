d764c9c7934d6cd00c8e5cdfb74cc90a52b15148
Chester Pineda <chesterpineda31@gmail.com>
2026-02-09 23:24:45 +0800

    Initial commit by Student Pineda

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-print("Lab 1: Version Control")
+print("Lab1: Version Control")
 print("Author: [Chester Pineda]")
 print("Student ID: TUPM-25-3692")
-
-print("Lab 1: Version Control")
-print("Author: Chester Pineda")
-print("Student ID: TUPM-25-3692")
-print("Class Section: [BSECE-1C]") #

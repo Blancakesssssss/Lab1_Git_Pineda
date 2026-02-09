f4057926fe4f335d77c45995f8f82e65b1ac095a
Chester Pineda <chesterpineda31@gmail.com>
2026-02-09 22:52:16 +0800

    Added section info for Madrigal.

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 print("Lab 1: Version Control")
 print("Author: [Chester Pineda]")
 print("Student ID: TUPM-25-3692")
+
+print("Lab 1: Version Control")
+print("Author: Chester Pineda")
+print("Student ID: TUPM-25-3692")
+print("Class Section: [BSECE-1C]") #
